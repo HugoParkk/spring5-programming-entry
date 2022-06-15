113f4b2eb79788c076010e599abaa5f02985a50c
박종연 <20041230andy@gmail.com>
2022-06-16 01:07:37 +0900

    20220615 chap 05 컴포넌트 스캔 p.140 chapter read

--- sp5-chap05/src/main/java/config/AppCtxWithExclude.java
+++ sp5-chap05/src/main/java/config/AppCtxWithExclude.java
@@ -4,11 +4,18 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import spring.*;
 
 @Configuration
-@ComponentScan(basePackages = {"spring"})
-public class AppCtx {
+//@ComponentScan(basePackages = {"spring"}, excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "spring\\..*Dao"))
+@ComponentScan(basePackages = {"spring"}, excludeFilters = @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "spring.*Dao"))
+public class AppCtxWithExclude {
+    @Bean
+    public MemberDao memberDao() {
+        return new MemberDao();
+    }
+
     @Bean
     @Qualifier("printer")
     public MemberPrinter memberPrinter1() {
